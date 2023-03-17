1c26b0211b4a05202e115f9d0619bd840e53f7e4
Vishal Pansare <51355863+vishalpansare5600@users.noreply.github.com>
2023-03-17 12:20:36 +0530

    Add files via upload

---
+++ sumof_number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num,i,sum=0;
+	printf("enter the positive number=");
+	scanf("%d",&num);
+
+
+for(i=0;i<=num;i++)
+{
+	sum=sum+1;
+	
+	}	
+	printf("sum of the first %dnumber is %d",num,sum);
+	getch();
+}
